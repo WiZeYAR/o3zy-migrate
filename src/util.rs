cf35c4fdec725673f290a7e6609c6b50755eaf72
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2022-09-19 09:04:36 +0200

    Add error logging

--- src/util.rs
+++ src/util.rs
@@ -13,13 +13,20 @@ pub enum Error {
 }
 
 pub fn run_cmd(cmd: &mut Command) -> Result<String, Error> {
-    let out = cmd.output().map_err(Error::IO).and_then(|out| {
-        if out.status.success() {
-            Ok(String::from_utf8_lossy(&out.stdout).to_string())
-        } else {
-            Err(Error::CMD(out))
-        }
-    });
+    let out = cmd
+        .output()
+        .map_err(Error::IO)
+        .and_then(|out| {
+            if out.status.success() {
+                Ok(String::from_utf8_lossy(&out.stdout).to_string())
+            } else {
+                Err(Error::CMD(out))
+            }
+        })
+        .or_else(|err| {
+            log::error!("{:#?}", err);
+            Err(err)
+        });
     log::trace!("{:#?}", out);
     out
 }
